1473f24f5bad773bd138c6ccc6c9a208f48dd5d2
Himanshu <himanshukrabc@gmail.com>
2024-09-18 14:56:38 +0530

    first commit

--- index.py
+++ index.py
@@ -65,4 +65,4 @@ async def mappings():
 # Run the app using uvicorn
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
