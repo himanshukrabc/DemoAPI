4c388f5945946c9da7c53ccac69e5c95bdcd7644
Himanshu <himanshukrabc@gmail.com>
2024-09-18 17:42:26 +0530

    FC

--- index.py
+++ index.py
@@ -38,27 +38,27 @@ async def mappings():
             "Provider": "DnB",
             "API_URL": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L1_V1",
             "Attribute_path": "organization.primaryName",
-            "provider_Attribute": "DnB Primary Name",
+            "provider_Attribute": "Company",
             "Data_type": "String"
         },
         {
             "Provider": "DnB",
             "API_URL": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L1_V1",
             "Attribute_path": "organization.registeredName",
-            "provider_Attribute": "DnB Registered Name",
+            "provider_Attribute": "RegisteredName",
             "Data_type": "String"
         },
         {
             "Provider": "DnB",
             "API_URL": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L4_V1",
             "Attribute_path": "organization.websiteAddress[0].url",
-            "provider_Attribute": "DnB Website",
+            "provider_Attribute": "CorporateWebsite",
             "Data_type": "url"
         },
         {
             "Provider": "DnB",
             "API_URL": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_advgeoposition_V1",
-            "Attribute_path": "oraganization.telephone[type=\"phone\"]",
+            "Attribute_path": "oraganization.telephone[0].telephoneNumber",
             "provider_Attribute": "address.PhoneNumber",
             "Data_type": "Number"
         },
@@ -72,7 +72,7 @@ async def mappings():
         {
             "Provider": "DnB",
             "API_URL": "v1/data/duns/{dunsNumber}?blockIDs=companyesgInsights_L4_V3",
-            "Attribute_path": "sum(organization.esgRanking.score)",
+            "Attribute_path": "organization.esgRanking.score",
             "provider_Attribute": "Score",
             "Data_type": "Number"
         }
