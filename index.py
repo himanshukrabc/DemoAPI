3f4ac118b85fa9cfef18f31f0b3c26b536240166
Himanshu <himanshukrabc@gmail.com>
2024-10-06 22:07:07 +0530

    Search

--- index.py
+++ index.py
@@ -48,92 +48,98 @@ async def dataProviders():
     }
 
 
-companies=[
-    {
-        "id": "5130efc38989846a360001f6",
-        "object_type": "company",
-        "name": "NetSuite",
-        "slug": "netsuite",
-        "dunsNumber": "754235132",
-        "url": "netsuite.com",
-        "location": "Redwood City, CA",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130efc38989846a360001f6.ico"
-    },
-    {
-        "id": "5130efc38989846a360001f6",
-        "object_type": "company",
-        "name": "Gorman",
-        "slug": "Gorman",
-        "dunsNumber": "804735132",
-        "url": "gorman.com",
-        "location": "Redwood City, CA",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130efc38989846a360001f6.ico"
-    },
-    {
-        "id": "5130efec8989846a36007762",
-        "object_type": "company",
-        "name": "Dyn",
-        "slug": "dyn",
-        "dunsNumber": "213489712",
-        "url": "www.dyn.com",
-        "location": "Manchester, NH",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130efec8989846a36007762.ico"
-    },
-    {
-        "id": "541abb8356a990ca1c000508",
-        "object_type": "company",
-        "name": "Sedgwick",
-        "slug": "sedgwick-1",
-        "dunsNumber": "239047824",
-        "url": "www.sedgwick.com",
-        "location": "Memphis, TN",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/541abb8356a990ca1c000508.ico"
-    },
-    {
-        "id": "5254d1049f309cd30400013a",
-        "object_type": "company",
-        "name": "Oracle Lighting",
-        "slug": "oracle-lighting",
-        "dunsNumber": "345987533",
-        "url": "oraclelights.com",
-        "location": "Metairie, LA",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5254d1049f309cd30400013a.ico"
-    },
-    {
-        "id": "5130f0248989846a3601359e",
-        "object_type": "company",
-        "name": "CrowdTwist",
-        "slug": "crowdtwist",
-        "dunsNumber": "354198734",
-        "url": "www.crowdtwist.com",
-        "location": "New York, NY",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130f0248989846a3601359e.ico"
-    },
-    {
-        "id": "5130f0598989846a3601e7a0",
-        "object_type": "company",
-        "name": "Moat",
-        "slug": "moat",
-        "dunsNumber": "135412344",
-        "url": "moat.com",
-        "location": "New York, NY",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130f0598989846a3601e7a0.ico"
-    },
-    {
-        "id": "58e22e3545930d2649ebd647",
-        "object_type": "company",
-        "name": "Oracle Corporation UK Limited",
-        "slug": "oracle-corporation-uk-limited",
-        "dunsNumber": "234512342",
-        "url": "www.oracle.com/uk",
-        "location": "Reading, United Kingdom",
-        "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/58e22e3545930d2649ebd647.ico"
-    }
-]
 @app.get("/autoSuggest")
-async def autoSuggest(searchString:str = Query(None)):
-    return {"res":{"results":[val for val in companies if searchString in val['name']]}}
+async def autoSuggest(searchString: str = Query(None)):
+    companies=[
+        {
+            "id": "5130efc38989846a360001f6",
+            "object_type": "company",
+            "name": "NetSuite",
+            "slug": "netsuite",
+            "dunsNumber": "754235132",
+            "url": "netsuite.com",
+            "location": "Redwood City, CA",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130efc38989846a360001f6.ico"
+        },
+        {
+            "id": "5130efc38989846a360001f6",
+            "object_type": "company",
+            "name": "Gorman",
+            "slug": "Gorman",
+            "dunsNumber": "804735132",
+            "url": "gorman.com",
+            "location": "Redwood City, CA",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130efc38989846a360001f6.ico"
+        },
+        {
+            "id": "5130efec8989846a36007762",
+            "object_type": "company",
+            "name": "Dyn",
+            "slug": "dyn",
+            "dunsNumber": "213489712",
+            "url": "www.dyn.com",
+            "location": "Manchester, NH",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130efec8989846a36007762.ico"
+        },
+        {
+            "id": "541abb8356a990ca1c000508",
+            "object_type": "company",
+            "name": "Sedgwick",
+            "slug": "sedgwick-1",
+            "dunsNumber": "239047824",
+            "url": "www.sedgwick.com",
+            "location": "Memphis, TN",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/541abb8356a990ca1c000508.ico"
+        },
+        {
+            "id": "5254d1049f309cd30400013a",
+            "object_type": "company",
+            "name": "Oracle Lighting",
+            "slug": "oracle-lighting",
+            "dunsNumber": "345987533",
+            "url": "oraclelights.com",
+            "location": "Metairie, LA",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5254d1049f309cd30400013a.ico"
+        },
+        {
+            "id": "5130f0248989846a3601359e",
+            "object_type": "company",
+            "name": "CrowdTwist",
+            "slug": "crowdtwist",
+            "dunsNumber": "354198734",
+            "url": "www.crowdtwist.com",
+            "location": "New York, NY",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130f0248989846a3601359e.ico"
+        },
+        {
+            "id": "5130f0598989846a3601e7a0",
+            "object_type": "company",
+            "name": "Moat",
+            "slug": "moat",
+            "dunsNumber": "135412344",
+            "url": "moat.com",
+            "location": "New York, NY",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/5130f0598989846a3601e7a0.ico"
+        },
+        {
+            "id": "58e22e3545930d2649ebd647",
+            "object_type": "company",
+            "name": "Oracle Corporation UK Limited",
+            "slug": "oracle-corporation-uk-limited",
+            "dunsNumber": "234512342",
+            "url": "www.oracle.com/uk",
+            "location": "Reading, United Kingdom",
+            "icon_url": "//axdprrulpfmh.compat.objectstorage.us-phoenix-1.oraclecloud.com/datafox-public-prod/favicons/58e22e3545930d2649ebd647.ico"
+        }
+    ]
+    # return {"res": {"results": companies}}
 
+    if searchString:
+        searchString = searchString.lower()  # Convert to lowercase for case-insensitive match
+        res = [val for val in companies if searchString in val['name'].lower()]
+        return {"res": {"results": res}}
+    else:
+        return {"res": {"results": []}}
 
 @app.get("/v1/data/duns/{dunsNumber}")
 async def companyDetails(dunsNumber:str, blockIDs:str = Query(None)):
@@ -143,89 +149,11 @@ async def companyDetails(dunsNumber:str, blockIDs:str = Query(None)):
         f = open('dataBlocks-sample (4).json')
         return json.load(f)[dunsNumber]
 
-@app.get("/search")
-async def mappings():
-    f = open('dataBlocks-sample (4).json')
-    return json.load(f)
 
 @app.get("/mappings")
 async def mappings():
-    return {
-        "mappings":[
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L1_V1",
-            "api_field_name": "organization.primaryName",
-            "internal_reg_attribute": "Company",
-            "supp_profile_attribute":"Y",
-            "data_type": "String"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L1_V1",
-            "api_field_name": "organization.duns",
-            "internal_reg_attribute": "DUNSNumber",
-            "supp_profile_attribute":"Y",
-            "data_type": "Number"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L1_V1",
-            "api_field_name": "organization.primaryAddress.addressCountry.isoAlpha2Code",
-            "internal_reg_attribute": "TaxpayerCountryCode",
-            "supp_profile_attribute":"Y",
-            "data_type": "Number"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L1_V1",
-            "api_field_name": "organization.registeredName",
-            "internal_reg_attribute": "RegisteredName",
-            "supp_profile_attribute":"Y",
-            "data_type": "String"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_L4_V1",
-            "api_field_name": "organization.websiteAddress[0].url",
-            "internal_reg_attribute": "CorporateWebsite",
-            "supp_profile_attribute":"Y",
-            "data_type": "url"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyInfo_advgeoposition_V1",
-            "api_field_name": "organization.telephone[0].telephoneNumber",
-            "internal_reg_attribute": "address.PhoneNumber",
-            "supp_profile_attribute":"Y",
-            "data_type": "Number"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyfinancials_L4_V2",
-            "api_field_name": "organization.otherFinancials[0].currency",
-            "internal_reg_attribute": "Currency",
-            "supp_profile_attribute":"Y",
-            "data_type": "String"
-        },
-        {
-            "mapping_id":1,
-            "ext_data_provider_code": "DnB",
-            "api_url": "v1/data/duns/{dunsNumber}?blockIDs=companyesgInsights_L4_V3",
-            "api_field_name": "organization.esgRanking.score",
-            "internal_reg_attribute": "Score",
-            "supp_profile_attribute":"Y",
-            "data_type": "Number",
-        }
-    ]}
-
+    f = open('mappings.json')
+    return json.load(f)
 
 # Run the app using uvicorn
 if __name__ == "__main__":
